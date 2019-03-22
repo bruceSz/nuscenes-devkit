19ea1432b6623b5ff850952fce7be080b5d92b50
Christopher Li <christopher.li@nutonomy.com>
2019-03-22 13:02:28 -0700

    * Removed Docker volume from testing pipeline.  Replaced with volume mount. * Added volume mount to kubernetes pod definition in Jenkinsfile.

--- setup/test_mini_split.sh
+++ setup/test_mini_split.sh
@@ -2,34 +2,30 @@
 dockerfile=$1
 set -eux
 
-data_image=registry-local.nutonomy.team:5000/nuscenes/mini_split_data
-data_container=mini_split_data
-data_volume=mini_split_volume
+unittest_container_name=test_container
+tutorial_container_name=tutorial_container
+image_name=test_mini_split
 
 function clean_up(){
     echo "Cleaning up docker containers and volumes if they already exist"
-    docker container stop ${data_container} || { echo "container does not exist"; }
-    docker container rm ${data_container} || { echo "container does not exist"; }
-    
-    docker rm -f test_container || { echo "test container does not exist"; }
-    docker volume rm ${data_volume} || { echo "volume does not already exist"; }
+    docker rm -f ${unittest_container_name} || { echo "test container does not exist"; }
+    docker rm -f ${tutorial_container_name} || { echo "tutorial container does not exist"; }
 }
 
 trap clean_up EXIT
 
 clean_up
 
-echo "Pulling image containing mini split data from registry"
-docker pull ${data_image} || { echo "error during docker pull;" exit 1; }
-
-echo "Creating Docker volume from container"
-docker run -d --name=${data_container} -v ${data_volume}:/data:ro ${data_image} || { echo "container already running";}
-
 echo "Building image containing nuscenes-devkit"
-docker build -t test_mini_split -f ${dockerfile} . || { echo "Failed to build main Docker image"; exit 1; }
+docker build -t ${image_name} -f ${dockerfile} . || { echo "Failed to build main Docker image"; exit 1; }
 
-docker run --name=test_container -v ${data_volume}:/data \
-    -e NUSCENES=/data/nuscenes-v1.0 test_mini_split \
-    /bin/bash -c "source activate nuenv && cd python-sdk && python -m unittest"
+# Run baseline unit tests.
+docker run --name=${unittest_container_name} -v /data:/data \
+    -e NUSCENES=/data/sets/nuscenes ${image_name} \
+    /bin/bash -c "set -eux; source activate nuenv && cd python-sdk && python -m unittest"
+
+# Test Jupyter notebook code
+docker run --name=${tutorial_container_name} -v /data:/data \
+    ${image_name} /bin/bash -c "setup/test_tutorial.sh"
 
 clean_up
