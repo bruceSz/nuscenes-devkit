056e6815c72fa9baacbb1fe25ecb35a4117bca91
kevintsok <Kk050793+>
2019-03-28 15:51:39 +0100

    try first time

---
+++ try.py
@@ -0,0 +1,20 @@
+from nuscenes.nuscenes import NuScenes
+
+nusc = NuScenes(version='v1.0-mini', dataroot='./data', verbose=True)
+my_sample = nusc.get('sample', 'cd9964f8c3d34383b16e9c2997de1ed0')
+#nusc.render_sample(my_sample['token'])
+
+sample_record = nusc.get('sample', my_sample['token'])
+#nusc.list_sample(my_sample['token'])
+for sd_token in sample_record['data'].values():
+    sd_record = nusc.get('sample_data', sd_token)
+    print(sd_record)
+
+print('annotations')
+for sa_token in sample_record['anns']:
+    sa_record = nusc.get('sample_annotation', sa_token)
+    print(sa_record)
+
+
+my_scene_token = nusc.field2token('scene', 'name', 'scene-0061')[0]
+#nusc.render_scene(my_scene_token)
