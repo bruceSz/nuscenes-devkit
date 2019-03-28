c6c77590ccf09459e90f5cdf8d34650ea552e482
kevintsok <Kk050793+>
2019-03-28 17:01:25 +0100

    try 2nd time

--- try.py
+++ try.py
@@ -1,4 +1,6 @@
+import os.path as osp
 from nuscenes.nuscenes import NuScenes
+from nuscenes.utils.data_classes import LidarPointCloud, RadarPointCloud, Box
 
 nusc = NuScenes(version='v1.0-mini', dataroot='./data', verbose=True)
 my_sample = nusc.get('sample', 'cd9964f8c3d34383b16e9c2997de1ed0')
@@ -8,7 +10,10 @@ sample_record = nusc.get('sample', my_sample['token'])
 #nusc.list_sample(my_sample['token'])
 for sd_token in sample_record['data'].values():
     sd_record = nusc.get('sample_data', sd_token)
-    print(sd_record)
+    if sd_record['sensor_modality'] == 'lidar':
+        pcl_path = osp.join(nusc.dataroot, sd_record['filename'])
+        pc = LidarPointCloud.from_file(pcl_path)
+        print(pc)
 
 print('annotations')
 for sa_token in sample_record['anns']:
